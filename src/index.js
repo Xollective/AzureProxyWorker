1fb59d1138e2b80511fe6850d39faee72532ff6a
Lance Collins <lancec@microsoft.com>
2025-08-18 15:36:00 -0700

    Fix  url

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ export default {
 		const url = new URL(request.url);
 		const [ organization, webhookName ] = url.pathname.split('/').filter(Boolean);
 
-		const azdevUrl = `POST https://dev.azure.com/${organization}/_apis/public/distributedtask/webhooks/${webhookName}?api-version=7.2-preview.2`;
+		const azdevUrl = `https://dev.azure.com/${organization}/_apis/public/distributedtask/webhooks/${webhookName}?api-version=7.2-preview.2`;
 
 		const signature = request.headers.get('X-Hub-Signature');
 		let sha1Prefix = '';
